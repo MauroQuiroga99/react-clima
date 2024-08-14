1a8e8fc918857653e9bcface8443b53843794e77
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-08-13 21:38:43 -0500

    colocar ciudad y país en el state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,21 @@
+import { ChangeEvent, useState } from "react";
+import { SearchType } from "../types";
 import { countries } from "../data/countries";
 
 const Form = () => {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div>
       <form className=" flex flex-col gap-8" action="">
@@ -11,19 +26,27 @@ const Form = () => {
           <input
             className=" p-2 bg-slate-400 font-bold  border-2 rounded-md border-white placeholder:text-white"
             type="text"
-            id="text"
+            id="city"
             name="city"
             placeholder="Ciudad"
+            value={search.city}
+            onChange={handleChange}
           />
         </div>
         <div className=" flex flex-col gap-8">
-          <label className="text-white font-bold uppercase" htmlFor="city">
+          <label className="text-white font-bold uppercase" htmlFor="country">
             País:
           </label>
-          <select className=" p-2 rounded-md bg-slate-400 text-white font-bold  border-2 border-white placeholder:text-white">
+          <select
+            onChange={handleChange}
+            value={search.country}
+            id="country"
+            name="country"
+            className=" p-2 rounded-md bg-slate-400 text-white font-bold  border-2 border-white placeholder:text-white"
+          >
             <option value="">--Seleccione un país--</option>
             {countries.map((country) => (
-              <option key={country.code} value={country.name}>
+              <option key={country.code} value={country.code}>
                 {country.name}
               </option>
             ))}
