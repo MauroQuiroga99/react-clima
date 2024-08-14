1c077f7734a6771dea02cd89207407f8e58f835d
Mauro Quiroga <148659366+MauroQuiroga99@users.noreply.github.com>
2024-08-13 19:51:37 -0500

    diseñar form

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Form from "./components/Form";
 function App() {
   return (
     <>
-      <h1 className=" text-center text-4xl text-white mt-10 font-bold ">
-        Buscador De Clima
-      </h1>
-      <div className="w-11/12 max-w-screen-lg m-auto text-white mt-10 md:grid md:grid-cols-2 md:items-center mdd:gap-5 md:mt-6">
-        <Form />
-        <p>2</p>
+      <div className="bg-[url('/public/bg-on-clima.gif')] bg-no-repeat bg-cover h-lvh  ">
+        <h1 className=" text-center text-4xl text-black uppercase py-10 mb-10 font-bold ">
+          Buscador De Clima
+        </h1>
+        <div className="w-11/12 max-w-screen-lg m-auto text-white mt-10 md:grid md:grid-cols-2 md:items-center md:gap-5 md:mt-6">
+          <Form />
+          <p>2</p>
+        </div>
       </div>
     </>
   );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,23 +3,25 @@ import { countries } from "../data/countries";
 const Form = () => {
   return (
     <div>
-      <form action="">
-        <div>
-          <label htmlFor="city">Ciudad:</label>
+      <form className=" flex flex-col gap-8" action="">
+        <div className=" flex flex-col gap-8">
+          <label className="text-white font-bold uppercase " htmlFor="city">
+            Ciudad:
+          </label>
           <input
-            className="text-black"
+            className=" p-2 bg-slate-400 font-bold  border-2 rounded-md border-white placeholder:text-white"
             type="text"
             id="text"
             name="city"
             placeholder="Ciudad"
           />
         </div>
-        <div>
-          <label htmlFor="city">País:</label>
-          <select name="" id="">
-            <option className="text-black" value="">
-              --Seleccione un país--
-            </option>
+        <div className=" flex flex-col gap-8">
+          <label className="text-white font-bold uppercase" htmlFor="city">
+            País:
+          </label>
+          <select className=" p-2 rounded-md bg-slate-400 text-white font-bold  border-2 border-white placeholder:text-white">
+            <option value="">--Seleccione un país--</option>
             {countries.map((country) => (
               <option key={country.code} value={country.name}>
                 {country.name}
@@ -27,7 +29,11 @@ const Form = () => {
             ))}
           </select>
         </div>
-        <input type="submit" value="Consultar Clima" />
+        <input
+          className=" p-5 bg-orange-500 text-white font-bold uppercase rounded-md border-2 border-white cursor-pointer"
+          type="submit"
+          value="Consultar Clima"
+        />
       </form>
     </div>
   );
